4fd3a9331f29d8d25ba06a46cfd10fa0ec50d959
Mike32332 <mtherrien@putnamscience.org>
2024-11-14 13:24:07 -0500

    Update import random.py

--- import random.py
+++ import random.py
@@ -1,1 +1,10 @@
-print('Welcome to your bookstore, what would you like to do?')
+class Book: 
+    def __init__(self, title, author, price, stock=1):
+        self.title = title
+        self.author = author 
+        self.price = price 
+        self.stock = stock 
+
+    def __str__(self):
+        return f'{self.title} by {self.author} - ${self.price} (Stock: {self.stock})'
+    def increse_price(self, percentage):
